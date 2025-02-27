577a190f131d2cc09f83abd84d61800398f5c7b8
sosaskze143 <sosaskze143@gmail.com>
2025-02-27 14:34:06 +0300

    Create instructions.js

---
+++ instructions.js
@@ -0,0 +1,8 @@
+function showInstructions() {
+    const instructions = document.getElementById('instructions');
+    instructions.innerHTML = `
+        <p>ضع إصبعك أمام الكاميرا بشكل واضح.</p>
+        <p>انتظر حتى يتم التقاط البصمة.</p>
+        <p>تحقق من جودة الإضاءة.</p>
+    `;
+}
